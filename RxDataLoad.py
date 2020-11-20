4d8949d4c7606cd7bbb3b08e9a242d590707eab4
Muwanguzi <smuwanguzi@msh.org>
2020-11-20 12:01:39 +0300

    Changing Rxdataload

--- RxDataLoad.py
+++ RxDataLoad.py
@@ -1258,7 +1258,7 @@ def updateLocalMedicines(Config):
 print (datetime.datetime.now())
 #update prodcut catalog
 updateLocalMedicines(Config)
-sendDataToRemoteServer(Config)
+#sendDataToRemoteServer(Config)
 #send prodct catalog update report
 #get email addresses to contacts
 cnxnMaster = pyodbc.connect('''DRIVER={%s};
