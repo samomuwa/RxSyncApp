738db1b98bc04cc0671ee30b660e167e40f12cbd
Muwanguzi <smuwanguzi@msh.org>
2020-11-20 12:12:18 +0300

    correcting log path

--- RxDataLoad.py
+++ RxDataLoad.py
@@ -21,7 +21,9 @@ path=os.path.dirname(os.path.realpath(__file__))+'/RxDataLoad.ini'
 #prepare logfile
 logFileName = datetime.datetime.today().date()
 logger = logging.getLogger(str(logFileName))
-hdlr = logging.FileHandler(str(os.path.dirname)+'\\logs\\%s.log'%(logFileName))
+#hdlr = logging.FileHandler(os.path.dirname(os.path.realpath(__file__))+'\\logs\\%s.log'%(logFileName))
+hdlr = logging.FileHandler('C:\\RxSyncApp\\logs\\%s.log'%(logFileName))
+
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 hdlr.setFormatter(formatter)
 logger.addHandler(hdlr) 
